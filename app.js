1a5d9c5c2f2421cd08c5a21ab85e845b9c000734
Doosik Kim <doosik@etri.re.kr>
2023-11-01 09:54:19 +0900

    Support params.

--- app.js
+++ app.js
@@ -6,45 +6,55 @@ import path from 'path';
 import { marked } from 'marked';
 import * as fs from 'node:fs';
 
-///////////////
-// Variables //
-///////////////
+//////////////////////
+// Global variables //
+//////////////////////
 
-const app = express();
+let docs_path;
 
-// Get port number from command line arguments.
-var argnum = parseInt(process.argv.slice(2)[0]);
-const port = isNaN(argnum) ? 80 : argnum;
+///////////////////
+// Main function //
+///////////////////
 
-// Set root path and docs path.
-var docs_path = process.argv.slice(3)[0];
-if (docs_path == undefined) {
-    docs_path = path.resolve() + '/docs';
-}
+function main() {
+    const app = express();
 
-app.set('views', docs_path);
-app.set('view engine', 'ejs');
+    // Get port number from command line arguments.
+    let argnum = parseInt(process.argv.slice(2)[0]);
+    const port = isNaN(argnum) ? 80 : argnum;
 
-app.get('/', (req, res) => {
-    res.redirect('/index.md');
-});
+    // Set root path and docs path.
+    docs_path = process.argv.slice(3)[0];
+    if (docs_path == undefined) {
+        docs_path = path.resolve() + '/docs';
+    }
 
-app.get('*.md', doc_handler);
+    app.set('views', docs_path);
+    app.set('view engine', 'ejs');
 
-app.get('*.__datetime__', datetime_handler);
+    app.get('/', (req, res) => {
+        res.redirect('/index.md');
+    });
 
-app.get('/search', search_handler);
+    app.get('*.md', doc_handler);
 
-app.get('*.mp4', mp4_handler);
+    app.get('*.__datetime__', datetime_handler);
 
-app.use(express.static(docs_path))
+    app.get('/search', search_handler);
 
-app.listen(port, () => {
-    const address = 'http://' + get_ip_address() + (port == 80 ? '' : `:${port}`) + '/';
-    console.log(`Server started on ${address} with docs_path=${docs_path}`);
-});
+    app.get('*.mp4', mp4_handler);
 
-app.get('*', default_handler);
+    app.use(express.static(docs_path))
+
+    app.get('*', default_handler);
+
+    app.listen(port, () => {
+        const address = 'http://' + get_ip_address() + (port == 80 ? '' : `:${port}`) + '/';
+        console.log(`Server started on ${address} with docs_path=${docs_path}`);
+    });
+};
+
+main();
 
 ///////////////////////
 // Handler functions //
@@ -63,16 +73,23 @@ const datetime_option = {
 function doc_handler(req, res) {
     try {
         const req_url = decodeURIComponent(req.url);
-        const page = new URL('http://127.0.0.1' + req_url).searchParams.get('page');
+        const params = new URL('http://127.0.0.1' + req_url).searchParams;
+        const page = params.get('page');
         const file_path = req_url.split('?')[0];
 
         console.log(`${(new Date()).toLocaleString('en-US', datetime_option)}, ${req.socket.remoteAddress}, ${req_url}`);
-        
-        var doc = fs.readFileSync(docs_path + file_path, 'utf8');
-        var title = doc.match(/^# .*?(?=\r?\n)/);
+
+        let doc = fs.readFileSync(docs_path + file_path, 'utf8');
+        let title = doc.match(/^# .*?(?=\r?\n)/);
         title = title ? title[0].substring(1).trim() : file_path;
-        var content = marked(doc, { "mangle": false, headerIds: false });
-        res.render('template.ejs', { "title": title, "content": content, "page": page });
+        let content = marked(doc, { "mangle": false, headerIds: false });
+
+        let dict_params = {};
+        params.forEach((value, key) => {
+            dict_params[key] = value;
+        });
+
+        res.render('template.ejs', { "title": title, "content": content, "page": page, "params": dict_params });
     } catch (error) {
         console.log(error);
         res.status(500).send('Internal error!');
@@ -84,8 +101,6 @@ function datetime_handler(req, res) {
         const req_url = decodeURIComponent(req.url);
         const file_path = req_url.substring(0, req_url.length - '.__datetime__'.length);
 
-        // console.log(`${(new Date()).toLocaleString('en-US', datetime_option)}, ${req.socket.remoteAddress}, ${req_url}`);
-        
         fs.stat(docs_path + file_path, function (err, stats) {
             if (err) {
                 res.status(400).send('Bad request!');
@@ -103,7 +118,7 @@ function search_handler(req, res) {
     try {
         const req_url = decodeURIComponent(req.url);
         const query = new URL('http://127.0.0.1' + req_url).searchParams.get('q');
-        var file_list = (query == null) ? [] : search_files(docs_path, query);
+        let file_list = (query == null) ? [] : search_files(docs_path, query);
 
         res.render('search.ejs', { "title": query, "content": file_list });
     } catch (error) {
@@ -130,9 +145,9 @@ function search_files(directory, query) {
 
                 if (extension !== '.md' && extension !== '.html')
                     continue;
-            
+
                 const content = fs.readFileSync(file_path, 'utf8').toLowerCase();
-                if (!content.includes(search_string)) 
+                if (!content.includes(search_string))
                     continue;
 
                 files_matching.push(file_path.substring(prefix_length).replace(/\\/g, '/'));
@@ -161,7 +176,7 @@ function mp4_handler(req, res) {
         if (range) {
             const [start, end] = range.replace(/bytes=/, '').split('-');
             const start_pos = parseInt(start);
-            var end_pos = parseInt(end);
+            let end_pos = parseInt(end);
             end_pos = isNaN(end_pos) ? video_size - 1 : end_pos;
             const content_length = end_pos - start_pos + 1;
 
--- docs/index.md
+++ docs/index.md
@@ -7,23 +7,44 @@
 
 ## Header 2
 
+### Header 3
+
+#### Header 4
+
+##### Header 5
+
+##### Header 6
+
+## Text
+
+- This is *emphasis*, **bold**, ***emphasis with bold***.
+- This is _emphasis_, __bold__, ___emphasis with bold___.
+
+## Links
+
 - [Subdir](subdir)
 - [Subdir without postfix](subdir/index)
 - [Subdir with full name](subdir/index.md)
 
-### Header 3
+## Mathjax
 
 - Math expression:
-$$
-1 \over \pi \tag*{Eq. 1.}
-$$
-- Inline math: $1 \over \pi$.
 
-#### Header 4
+$$
+1 \over \pi \tag{1}
+$$
+
+$$
+x_i + y_j = a_i + b_j
+$$
+
+- Inline math: $1 \over 2$.
+
+## Code Block
+
+- Code
 
 ```javascript
-// This is javascript
-
 import express from 'express';
 
 const app = express();
@@ -32,10 +53,10 @@ app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 });
 ```
+
+- Text
 
 ```text
-// This is text
-
 import express from 'express';
 
 const app = express();
@@ -44,12 +65,10 @@ app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 });
 ```
-
-##### Header 5
 
 - Inline code: `ENTER`.
 
-##### Header 6
+## Table
 
 | Table Caption |
 | ------------- |
